24eac17687c908953252c1f84da26948f60228df
Alfons Bengtegård <alfons.bengtegard@iusinnovation.se>
2018-05-19 08:54:43 +0200

    implement fcm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Platform, Toast } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { ToastController } from 'ionic-angular';
+import { Subject } from 'rxjs/Subject';
+import { tap } from 'rxjs/operators'
+import { FcmProvider } from '../providers/fcm/fcm';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +15,39 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    private platform: Platform,
+    private statusBar: StatusBar,
+    private splashScreen: SplashScreen,
+    private fcm: FcmProvider,
+    private toastCtrl: ToastController
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      // Get a FCM token
+      this.fcm.getToken()
+
+      // Listen to incoming messages
+      this.fcm.listenToNotifications().pipe(
+        tap(msg => {
+          // show a toast
+          const toast = toastCtrl.create({
+            message: msg.body,
+            duration: 3000
+          });
+          toast.present();
+        })
+      )
+      .subscribe()
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
+  
+
+  
+
 }
--- src/providers/fcm/fcm.ts
+++ src/providers/fcm/fcm.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Firebase } from '@ionic-native/firebase';
+import { Platform } from 'ionic-angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+
 
 /*
   Generated class for the FcmProvider provider.
@@ -10,8 +14,46 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FcmProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+    public http: HttpClient,
+    private firebaseNative: Firebase,
+    private platform: Platform,
+    private afs: AngularFirestore
+  ) {
     console.log('Hello FcmProvider Provider');
+  }
+
+  async getToken() {
+
+    let token;
+  
+    if (this.platform.is('android')) {
+      token = await this.firebaseNative.getToken()
+    } 
+  
+    if (this.platform.is('ios')) {
+      token = await this.firebaseNative.getToken();
+      await this.firebaseNative.grantPermission();
+    } 
+    
+    return this.saveTokenToFirestore(token)
+  }
+
+  private saveTokenToFirestore(token) {
+    if (!token) return;
+  
+    const devicesRef = this.afs.collection('devices')
+  
+    const docData = { 
+      token,
+      userId: 'testUser',
+    }
+  
+    return devicesRef.doc(token).set(docData)
+  }
+
+  listenToNotifications() {
+    return this.firebaseNative.onNotificationOpen()
   }
 
 }
