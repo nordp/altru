4764d34e039a67b02e9771f4e1688748b1823756
Alfons Bengtegård <alfons.bengtegard@iusinnovation.se>
2018-05-19 17:37:50 +0200

    search fixes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Campaign } from '../../models/campaign';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'page-home',
@@ -11,12 +12,55 @@ import 'rxjs/add/operator/map';
 })
 export class HomePage {
   items: Observable<Campaign[]>;
+  filter: string = '';
+
   constructor(
     public navCtrl: NavController,
     public db: AngularFirestore,
   ) {
-    this.items = db.collection<Campaign>('campaigns').snapshotChanges().map(actions => {
-      return actions.map(a => {
+    // this.items = db.collection<Campaign>('campaigns')
+    // .snapshotChanges()
+    // .map(actions => {
+    //   return actions
+    //     .filter(campaign => {
+    //       const data = campaign.payload.doc.data() as Campaign;
+    //       const tagMatch = data.tags.some(tag => tag === filter);
+    //       const nameMatch = true;
+    //       return tagMatch || nameMatch;
+    //     })
+    //     .map(a => {
+    //     const data = a.payload.doc.data() as Campaign;
+    //     const id = a.payload.doc.id;
+    //     return { id, ...data };
+    //   });
+    // });
+    this.getItems();
+  }
+
+  getItems() {
+    this.items = this.db.collection<Campaign>('campaigns')
+    .snapshotChanges()
+    .map(actions => {
+      return actions
+        .filter(campaign => {
+          console.log('hej');
+          if (this.filter) {
+            const regexp = new RegExp(`/${this.filter.toLowerCase()}.*/`, 'g');
+            const data = campaign.payload.doc.data() as Campaign;
+            const tagMatch = data.tags.some(tag => {
+              console.log(tag.match(regexp))
+              console.log(this.filter);
+              return tag.match(regexp) !== null
+            });
+            const nameMatch = data.name.toLowerCase().match(regexp) !== null;
+            console.log(tagMatch);
+            console.log(nameMatch);
+            return tagMatch || nameMatch;
+          } else {
+            return true;
+          }
+        })
+        .map(a => {
         const data = a.payload.doc.data() as Campaign;
         const id = a.payload.doc.id;
         return { id, ...data };
