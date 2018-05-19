88f36012f7660fd0dd7ad54a92b9a0f512674a42
Philip Nord <nordp@student.chalmers.se>
2018-05-19 08:55:37 +0200

    Added firestore working example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  items: Observable<any[]>;
+  constructor(
+    public navCtrl: NavController,
+    public db: AngularFirestore,
+  ) {
+    this.items = db.collection('items').valueChanges();
   }
 
 }
